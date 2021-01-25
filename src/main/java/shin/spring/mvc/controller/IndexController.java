f5d1c8beff3a1c3dd20806f7da33a25181e18a8f
jumprope123 <70045183+jumprope123@users.noreply.github.com>
2021-01-25 12:10:46 +0900

    엔터프라이즈급을 위한 뼈대

---
+++ src/main/java/shin/spring/mvc/controller/IndexController.java
@@ -0,0 +1,13 @@
+package shin.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index.tiles";
+    }
+}
